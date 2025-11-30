18fa96b70d488abfc184266b8a64616f409f2fdc
Henryawd345 <henrygoodboy2000@gmail.com>
2025-11-30 03:03:23 -0800

    Testing (Milestone 2 complete)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
+    IntBinaryTree tree;
+
+    tree.insertNode("B20");
+    tree.insertNode("A5");
+    tree.insertNode("C9");
+    tree.insertNode("@31");
+
+    tree.displayInOrder();
 
     return 0;
 }
